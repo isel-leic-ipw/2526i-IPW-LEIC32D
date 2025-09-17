c720a84f1a13759878622112a1437729aea23798
fbfreitas <ffreitas@cc.isel.ipl.pt>
2025-09-17 15:18:40 +0100

    aula4

---
+++ aula4/constructor.js
@@ -0,0 +1,22 @@
+function Student(name, number ){
+    console.log(this)
+    this.number = number
+    this.name = name
+    console.log(this)
+}
+
+const std1 = new Student("Filipe", 1234)
+const std2 = new Student("Joao", 4321)
+
+
+
+//std1.f = function() {console.log("F")}
+//Student.prototype.f = function() {console.log("F")}
+Object.prototype.f = function() {console.log(this)}
+
+std1.f() 
+std2.f() 
+
+const ar = [1,2]
+ar.f()
+
